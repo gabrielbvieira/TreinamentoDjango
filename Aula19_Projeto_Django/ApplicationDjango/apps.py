9cfa072464248020cc718f7e5f40cdf50ef8267d
Gabriel Vieira <gabrielbvieira08@gmail.com>
2023-09-17 22:18:47 -0300

    17/09 pt.2

--- Aula19_Projeto_Django/ApplicationDjango/apps.py
+++ Aula19_Projeto_Django/ApplicationDjango/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AplicationdjangoConfig(AppConfig):
+class ApplicationdjangoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AplicationDjango'
+    name = 'ApplicationDjango'
--- Aula19_Projeto_Django/ApplicationDjango/views.py
+++ Aula19_Projeto_Django/ApplicationDjango/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'curso': 'Treinamento Django'
+    }
+    return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
