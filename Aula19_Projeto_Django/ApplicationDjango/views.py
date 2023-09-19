9c64a80d3f9cbbbc7748cadc67710089831004c5
Gabriel Vieira <gabrielbvieira08@gmail.com>
2023-09-19 13:29:22 -0300

    19-09
    .

--- Aula19_Projeto_Django/ApplicationDjango/views.py
+++ Aula19_Projeto_Django/ApplicationDjango/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
+from .models import Produto
 
 def index(request):
-    if str(request.user) == 'AnnonymousUser':
-        teste = 'Usuário não logado'
-    else:
-        teste = 'Usuário logado'
-    
+    produtos = Produto.objects.all()
     context = {
         'curso': 'Treinamento Django',
-        'logado': teste
+        'produtos' : produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, id):
+    print(f'ID: {id}')
+    produto = Produto.objects.get(id=id)
+    context = {
+        'produto': produto
+    }
+    return render(request, 'produto.html', context)
